0d2db87ca8084a15101015e552b70edae96c09a7
unknown <patilrutvik501@gmail.com>
2025-11-20 18:25:39 +0530

    last commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  APIURL: 'http://localhost:5000'   // your API base URL
+  APIURL: 'http://localhost:5000'   //  Local API base URL
+  // APIURL: 'https://arvog-server.onrender.com'   //  Live API base URL
 };
